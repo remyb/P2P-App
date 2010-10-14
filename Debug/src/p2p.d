5950a91c8bfcd4bbfb6df5a9d61a768255575e25
Remy Baumgarten <remy.baumgarten@gmail.com>
2010-10-13 19:09:05 -1000

    project setup

---
+++ Debug/src/p2p.d
@@ -0,0 +1,1 @@
+src/p2p.d src/p2p.o: ../src/p2p.c
---
+++ Debug/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/p2p.c 
+
+OBJS += \
+./src/p2p.o 
+
+C_DEPS += \
+./src/p2p.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
