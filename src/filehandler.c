9439b0e460dd2f84d82d77642a74abc41cde3871
Remy Baumgarten <remy.baumgarten@gmail.com>
2010-11-03 16:46:25 -1000

    added file handler functions

--- src/filehandler.c
+++ src/filehandler.c
@@ -1,3 +1,6 @@
+#include "filehandler.h"
+
+/** Count files in directory **/
 short countfiles() {
     char systemcall[100];
     sprintf(systemcall,"ls ./content | wc -l");
@@ -15,3 +18,59 @@ short countfiles() {
     pclose(fp);
     return k;
 }
+
+/** check to see if file exists **/
+int file_exist (char *filename)
+{
+  struct stat buffer;   
+  return (stat (filename, &buffer) == 0);
+}
+
+/** print out list of files **/
+int getFileList(char *path) {
+    DIR *dp;
+    struct dirent *ep;
+    dp = opendir (path);
+    if (dp != NULL)
+    {
+        while (ep = readdir(dp))
+        puts(ep->d_name);  // output, prints to screen here
+            (void) closedir(dp);
+    }
+    else
+        perror ("Couldn't open the directory");
+    return 0;
+}
+
+/** delete item in directory **/
+int deleteContent(char *filename){
+    if (remove(filename) == -1) {
+        printf("file not found for removal\n");
+        return 0;
+    } 
+    return 1;
+}
+
+/** testing **/
+/*
+int main () {
+    
+    printf("\n\n==Testing if File Exists==\n\n");
+    if(file_exist("stamp-h1a")) 
+        printf("file exists\n");
+    else
+        printf("file does NOT exist\n");
+    
+    printf("\n\n===testing listing contents of directory==\n\n");
+    printf("%d\n", getFileList("/tmp"));
+    
+    printf("\n\n===counting # of files of directory==\n\n");
+    printf("Number of Files in Directory: %d\n\n", countfiles());
+    
+    printf("\n\n===Deleting file===\n\n");
+    if (deleteContent("temp.txt")) printf("file was successfully deleted...\n");
+    else printf("file count not be deleted....\n");
+    printf("done...\n\n");
+}
+*/
+
--- src/filehandler.h
+++ src/filehandler.h
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
 
 short countfiles();
+int file_exist (char *filename);
+int getFileList(char *path);
+int deleteContent(char *filename);
